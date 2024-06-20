49f57916ca3e9cf93a85f443b6caf8bbac41061a
Shodmon0406 <133884946+Shodmon0406@users.noreply.github.com>
2024-06-20 09:18:42 +0500

    Added product discount

---
+++ Keeper.Core/Migrations/20240612090524_RemoveDescriptionInCategory.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Keeper.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveDescriptionInCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                schema: "new-keeper",
+                table: "Categories");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ParentId",
+                schema: "new-keeper",
+                table: "Categories",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ParentId",
+                schema: "new-keeper",
+                table: "Categories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                schema: "new-keeper",
+                table: "Categories",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: true);
+        }
+    }
+}
